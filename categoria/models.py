61223320bfd8c097da44997f5717cb16b23db9a6
Vaz15K <keveneduardobilibio@hotmail.com>
2026-03-05 14:10:30 -0400

    Adiciona método __str__ à classe Categoria

--- categoria/models.py
+++ categoria/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
 class Categoria(models.Model):
     nome = models.CharField(max_length=50, unique=True, blank=False)
     descricao = models.TextField(max_length=150, blank=False)
     imagem = models.ImageField(upload_to="fotos/categoria", blank=True)
     slug = models.SlugField(max_length=90, unique=True)
+
+    def __str__(self):
+        return self.nome
